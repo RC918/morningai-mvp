19a9eb7acce5e5749176499c21fc3a31e6064c85
RC918 <127590951+RC918@users.noreply.github.com>
2025-09-18 14:48:10 -0400

    feat(format): [PR B] auto-format code with autoflake, isort, and black

--- apps/api/src/database.py
+++ apps/api/src/database.py
@@ -1,19 +1,16 @@
+import os
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
 
 # 初始化 SQLAlchemy
 db = SQLAlchemy()
 
 # 設置資料庫連接
 DATABASE_URL = os.environ.get(
-    'DATABASE_URL',
-    f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
+    "DATABASE_URL", f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
 )
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
-
